d25ffc91796fafe64e9eaab07eba5963858a6d64
Emmanuel WALDNER <emmanuel.waldner@gmail.com>
2021-04-05 21:12:38 +0200

    Passage a 4 Images

--- static/api.js
+++ static/api.js
@@ -1,5 +1,41 @@
 let url = "http://127.0.0.1:8000/api/v1/titles/";
 
+class TopMovie{
+  constructor(url) {
+    this.url = url + '?&sort_by=-imdb_score' ;
+    this.movie = "" ;
+  }
+
+  Retrieve(){
+    return axios.get(this.url)
+        .then( (res) => {
+      let data = res.data.results[0];
+      let first_movie = new Movie(data.id, data.image_url, data.title);
+      first_movie.retrieve_info().then( () =>
+          {
+            this.movie = first_movie;
+            this.Display();
+          });
+      });
+  }
+
+
+  Display() {
+      let x = document.getElementsByClassName("container_image");
+      let img = document.createElement("img");
+      img.src = this.movie.url_image;
+      console.log(x);
+      x[0].appendChild(img);
+      let info = document.createElement("div");
+      info.innerHTML = this.movie.description;
+      info.classList = "container_description";
+      x[0].parentNode.insertBefore(info);
+
+
+    }
+}
+
+
 
 class Categorie{
   constructor(name=String) {
@@ -16,20 +52,20 @@ class Categorie{
         this.next = response.data.next;
         for (let i in response.data.results) {
           let data = response.data.results[i];
-          let film = new Movie(data.id, data.image_url, data.title, data.genres);
+          let film = new Movie(data.id, data.image_url, data.title);
           this.list_movies.push(film);
         }
-        try {
-          const response = await axios.get(this.next);
-            for (let i = 0; i < 2; i++){
-              let data = response.data.results[i];
-              let film = new Movie(data.id, data.image_url, data.title, data.genres);
-              this.list_movies.push(film);
-            }
-        }
-        catch (error){
-          console.error(error + " Error on second scan");
-        }
+          try {
+            const response = await axios.get(this.next);
+              for (let i = 0; i < 2; i++){
+                let data = response.data.results[i];
+                let film = new Movie(data.id, data.image_url, data.title, data.genres);
+                this.list_movies.push(film);
+              }
+          }
+          catch (error){
+            console.error(error + " Error on second scan");
+          }
 
       }
       catch (error) {
@@ -44,38 +80,27 @@ class Categorie{
     for (let i = 0; i < this.list_movies.length; i++) {
       let x = document.getElementsByClassName(this.name + "-item" + (i + 1));
       let img = document.createElement("img");
-
-      // x[0].appendChild(a) = url + this.list_movies[i].id;
+      let a = document.createElement("a");
+      a.setAttribute('href', url + this.list_movies[i].id);
       img.src = this.list_movies[i].url_image;
-      x[0].appendChild(img);
+      x[0].appendChild(a);
+      a.appendChild(img);
     }
   }
 
 }
 
 class Movie{
-  constructor(id=String, url_image=String, title=String, genre=Array){
+  constructor(id=String, url_image=String, title=String){
     this.id = id;
     this.url_image = url_image;
     this.title = title;
-    this.genre = genre;
-    this.release_date = "";
-    this.rate = "";
-    this.imdb_score = "";
-    this.realisator = "";
-    this.actors = "";
-    this.time = "";
-    this.country = "";
-    this.result = "";
-    this.description = "";
   }
 
   retrieve_info() {
-    axios.get(url + this.id)
+    return axios.get(url + this.id)
     .then((response) =>  {
-      this.url_image = response.data.image_url;
-      this.title = response.data.title;
-      this.genre = response.data.genres;
+      this.genres = response.data.genres
       this.release_date = response.data.year;
       this.rate = response.data.rated;
       this.imdb_score = response.data.imdb_score;
@@ -85,6 +110,7 @@ class Movie{
       this.country = response.data.countries;
       this.result = response.data.reviews_from_critics;
       this.description = response.data.description;
+      this.long_description = response.data.long_description;
           });
   }
 
@@ -95,6 +121,10 @@ let Action = new Categorie("Action");
 let SciFi = new  Categorie("Sci-Fi");
 let Adventure = new Categorie("Adventure");
 let all = new Categorie("");
+let first = new TopMovie(url);
+
+//console.log(Topmovie);
+first.Retrieve();
 
 Action.Information();
 SciFi.Information();
